6f0bc6e409ba9f288a60897a20c471cb1470dd1c
Jesse Tippit <jtippit223@gmail.com>
2019-11-19 05:56:17 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -46,5 +46,5 @@ def using_delete(instructors, no_offense_steven)
    no_offense_steven = instructors.delete("Steven")
 end
 def using_delete_at(famous_robots, deleted_robot)
-  deleted_robot = famous_robots.delete(2)
+  deleted_robot = famous_robots.delete(integer)
  end
